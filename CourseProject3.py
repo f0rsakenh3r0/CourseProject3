5fad8616ca31e50a7dc8acc966f5c0d18911bc95
f0rsakenh3r0 <dann1@live.com>
2022-11-17 17:26:56 -0800

    .

--- CourseProject3.py
+++ CourseProject3.py
@@ -120,7 +120,7 @@ if __name__ == "__main__":
         # write the line of code that will concatenate fromdate, todate, empname, hours, hourlyrate, and taxrate. Pipe delimit each value and add a carriage return to the end of the line
         # and assign the line to EmpDetail
         
-        EmpDetail = EmpList.join(fromdate, '|', todate, '|', empname, '|', str(hours), '|', str(hourlyrate), '|', str(taxrate), '\n')
+        EmpDetail = EmpList.join(fromdate, '|', todate, '|', empname, '|', str(hours), '|', str(hourlyrate), '|', str(taxrate), '\n') 
  
         # write the line of code that will write EmpDetail to EmpFile
         EmpFile = EmpDetail
